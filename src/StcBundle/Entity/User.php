01a709a9fcc33255a8fb43454f771887f9c6ba26
SkyBaud <baudier.dorian@gmail.com>
2017-03-07 15:35:19 +0100

    Création de l'entité User Création de la base de données

---
+++ src/StcBundle/Entity/User.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace StcBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="users")
+ */
+class User {
+
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(name="id",type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(name="login",type="string", length=20, nullable=false)
+     */
+    private $login;
+
+    /**
+     * @ORM\Column(name="password",type="string", length=100, nullable=false)
+     */
+    private $password;
+
+    /**
+     * @ORM\Column(name="mail",type="string", length=100, nullable=false)
+     */
+    private $mail;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set login
+     *
+     * @param string $login
+     *
+     * @return User
+     */
+    public function setLogin($login)
+    {
+        $this->login = $login;
+
+        return $this;
+    }
+
+    /**
+     * Get login
+     *
+     * @return string
+     */
+    public function getLogin()
+    {
+        return $this->login;
+    }
+
+    /**
+     * Set password
+     *
+     * @param string $password
+     *
+     * @return User
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Get password
+     *
+     * @return string
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * Set mail
+     *
+     * @param string $mail
+     *
+     * @return User
+     */
+    public function setMail($mail)
+    {
+        $this->mail = $mail;
+
+        return $this;
+    }
+
+    /**
+     * Get mail
+     *
+     * @return string
+     */
+    public function getMail()
+    {
+        return $this->mail;
+    }
+}
