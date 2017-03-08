07b8314c402dbb0f5e5f0541904c499cb0d95076
Yoshi <kosmixer@gmail.com>
2017-03-08 16:34:18 +0100

    commit final fonctionnalité connexion utilisateur

--- src/StcBundle/Form/UserType.php
+++ src/StcBundle/Form/UserType.php
@@ -5,6 +5,7 @@ namespace StcBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type as FormType;
 
 class UserType extends AbstractType {
 
